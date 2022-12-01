8b3649bd1f5bc4156cf616bb5e1c36fb359a2ebb
beamlakTadesse01 <thecmboy930@gmail.com>
2022-12-01 06:58:40 -0500

    made the appending method recursive but not completly working: checkpoint

--- DS.js
+++ DS.js
@@ -1,50 +1,67 @@
-class Node {
-    constructor(data) {
+class Node{
+    constructor(data){
         this.data = data
         this.next = null
         this.prev = null
     }
 }
 
-class Branch {
-    constructor() {
+class Branch{
+    constructor(){
         this.head = null
     }
 
-    get is_empty() {
+    get is_empty(){
         return this.head === null
     }
 
-    append({ parent, data }) {
+    append({parent,data}){
         const node = new Node(data)
-        if (this.is_empty) {
-            node.prev = this.head
-            return this.head = node
-        }
+        const is_empty = this.is_empty
 
-        if (parent) {
-            const search = this.search(parent)
-            node.prev = search
-
-            if (!search.child) {
-                return search.child = node
-            }
-
-            let current = search.child
-            while (current) {
-                if (current.next === null) {
-                    node.prev = current
-                    return current.next = node
-                }
-                current = current.next
-            }
-            return
-        }
+        if(is_empty)return this.head = node
 
         let current = this.head
+        while(current){
 
-        while (current) {
-            if (current.next === null) {
+            if(parent){
+                // console.log(current,'current')
+                // console.log(current)
+                if(current.data === parent){
+                    if(current.child){
+                        console.log('\n')
+                        console.log(`%c PARENT NODE => ${current.data}\n\tGot Additional New \n\t\t CHILD NODE => ${data}`,'color : blue')
+                        
+                        current = this.search({
+                            node :current.child
+                        })
+                        return current.next = node
+                    }else{
+                        console.log('\n')
+                        console.log(`%c PARENT NODE => ${current.data}\n\t Got First \n\t\t CHILD NODE => ${data}`,'color : green')
+                        
+                        node.prev = current
+                        return current.child = node
+                    }
+                }else if (current.child){
+                    // !Over this block is where it needs some thinking
+                    let child = current.child
+                    
+                    console.log(child)
+                    const search = this.search({data : parent,node : child})
+                    if(!search){
+                        // console.log(current.child)
+                        current = child
+                        continue
+                    }
+                    current = search
+                    node.prev = search
+                    continue
+                    return search.next = node
+                }
+            }
+
+            if(!current.next){
                 node.prev = current
                 return current.next = node
             }
@@ -52,36 +69,13 @@ class Branch {
         }
     }
 
-    search(data) {
-        let current = this.head
-        let tmp
-        while (current) {
-            // console.log(current)
-            if (current.data === data) {
-                return current
-            } else if (current.child) {
-                // console.log(Boolean(n.next))
+    search({data, node}){
+        let current = node
 
-                tmp = current
-                current = current.child
-
-                if (Boolean(current.next)) {
-                    let n = current
-                    console.log(n.next === null)
-                    while (n) {
-                        // console.log(n,'NExt')
-                        if (n.data === data) {
-                            return n
-                        }
-                        n = n.next
-                    }
-                }
-                continue
-
-            } else if (!current.next) {
-                current = tmp
-                continue
-            }
+        while(current){
+            // console.log(current,data)
+            if(data && current.data === data) return current
+            if(!data && current.next === null)return current
             current = current.next
         }
 
@@ -90,65 +84,140 @@ class Branch {
 }
 
 const b = new Branch()
-b.append({ data: 'Root' })
 b.append({
-    data: 'CS Development'
-})
-b.append({
-    parent: 'CS Development',
-    data: 'PHD Stud'
-})
-b.append({
-    parent: 'CS Development',
-    data: 'Professors'
-})
-b.append({
-    parent: 'PHD Stud',
-    data: 'Stud1'
-})
-b.append({
-    parent: 'PHD Stud',
-    data: 'Stud2'
-})
-b.append({
-    parent: 'PHD Stud',
-    data: 'Stud3'
-})
-b.append({
-    parent: 'Stud1',
-    data: 'Name'
-})
-b.append({
-    parent: 'Stud1',
-    data: 'Papers'
-})
-b.append({
-    parent: 'Papers',
-    data: 'pap1'
-})
-b.append({
-    parent: 'pap1',
-    data: 'Title'
-})
-b.append({
-    parent: 'pap1',
-    data: 'Author'
-})
-b.append({
-    parent: 'Stud2',
-    data: 'name'
+    data:0
 })
 
 b.append({
-    parent: 'Stud3',
-    data: 'name1'
+    data:1
 })
 
 b.append({
-    parent: 'Stud3',
-    data: 'pap2'
+    parent : 1,
+    data : 2
 })
-console.log('searching stud3')
-const s = b.search('Stud3')
-console.log(s)
+
+
+b.append({
+    parent : 1,
+    data : 3
+})
+
+b.append({
+    parent : 2,
+    data : 4
+})
+
+b.append({
+    parent : 2,
+    data : 5
+})
+
+b.append({
+    parent : 2,
+    data : 6
+})
+// console.warn('appending sub child')
+
+b.append({
+    parent : 3,
+    data : 7
+})
+
+b.append({
+    parent : 3,
+    data : 8
+})
+
+// console.warn('appending sub child')
+
+b.append({
+    parent : 4,
+    data: 9
+})
+
+b.append({
+    parent : 4,
+    data: 10
+})
+
+b.append({
+    parent : 5,
+    data : 11
+})
+
+b.append({
+    parent : 6,
+    data : 12
+})
+
+b.append({
+    parent : 6,
+    data : 13
+})
+console.error('After this line Needs a fix and it HAve to do in block code startig from line 46')
+// console.warn('appending sub child')
+
+b.append({
+    parent : 7,
+    data : 14
+})
+
+// b.append({
+//     parent : 7,
+//     data : 15
+// })
+
+// b.append({
+//     parent : 8,
+//     data : 16
+// })
+
+// b.append({
+//     parent : 8,
+//     data : 17
+// })
+
+// b.append({
+//     parent : 10,
+//     data : 18
+// })
+
+// b.append({
+//     parent : 13,
+//     data : 19
+// })
+// // console.warn('appending sub child')
+
+// b.append({
+//     parent : 15,
+//     data : 20
+// })
+
+// b.append({
+//     parent : 18,
+//     data : 21
+// })
+
+// b.append({
+//     parent : 18,
+//     data : 22
+// })
+
+// b.append({
+//     parent : 19,
+//     data : 23
+// })
+
+// b.append({
+//     parent : 19,
+//     data : 24
+// })
+
+// console.warn('appending sub child')
+
+// b.append({
+//     parent : 20,
+//     data : 25
+// })
 console.log(b)
