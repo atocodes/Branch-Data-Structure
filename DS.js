93d0e721784827e15304c82b705b5ddfbff8b1c6
beamlakTadesse01 <thecmboy930@gmail.com>
2022-12-05 03:45:40 -0500

    loop_through method change and id for every node
    search()
    it used to be sub method called loop_through() in order to find
    and return the parent node for the next child node but now i made a
    search algorythm to search and return the parent node so now its
    possible to create a child for the specific parent.
    
    ID
    now every parent and childern node have a specific id.
    the main use abt the id is there a much possibility that there will
    be same datas in the branch and in order to reduce the possibility of
    unwanted mis getting the parent node i have provided an id for
    every nodes.
    
    CONCLUSION
    now so it is possible to search by id or by the name of the data.
    best recomanded is to search by ID.
    
    FINALLY
    i belive the data structure so far is working as expected to be
    emplimented on the web app project i am currently working on called
    BRANCH it has a couple of methods and configurations the needed to be
    done. so far its a big step
    
    BEAMLAK TADESSE || MHCDA

--- DS.js
+++ DS.js
@@ -10,18 +10,23 @@ class Branch{
     constructor(){
         this.head = null
     }
-
     get is_empty(){
         return this.head === null
     }
-
+    
     append({parent,data}){
         const node = new Node(data)
         const is_empty = this.is_empty
+        let count = 1
+        
         console.log('Parent',parent,'Data',data)
+        
         if(is_empty){
             console.log(`%c Got First Head \n\t\t Node Branch => ${data} : SUCCESS ✔`,'color : blueviolet')
-            return this.head = node}
+            node.id = 0
+            return this.head = node
+        }
+
         let current = this.head
 
         const path_viewer = (branch) =>{
@@ -46,59 +51,75 @@ class Branch{
             return arr
         }
 
+        const console_view = ({parent,node,child=true,new_child=true}) =>{
+            if(child){
+                if(new_child){
+                    console.log(`%cPARENT NODE => ${parent.data}\n\tGot New \n\t\t CHILD NODE => ${data} : SUCCESS ✔`,'color : green')
+                }else{
+                    console.log(`%cPARENT NODE => ${parent.data}\n\tGot Additional New \n\t\t CHILD NODE => ${data} : SUCCESS ✔`,'color : blue')
+                }
+            }else{
+                console.log(`%cBRANCH \n\tGot Additional New \n\t\t Next NODE => ${data} : SUCCESS ✔`,'color : blue')
+            }
+
+            const p = path_viewer(node)
+            console.log(`%cPATH: ${p}`,'color:gray')
+            console.log(parent)
+        }
+
         const make_node = ({parent,new_node}) =>{
-            
+            let parent_id = parent.id
             if(parent.child){
-                const s = this.search({node : parent.child})
-                new_node.prev = s
+                let s = parent.child
+                let child_id
+                const nums = s.id.replace(/-/,' ').split('').map(e => parseInt(e))
+                child_id = nums[nums.length-1]
 
-                // * CONSOLE VIEW
-                console.log(`%c PARENT NODE => ${parent.data}\n\tGot Additional New \n\t\t CHILD NODE => ${data} : SUCCESS ✔`,'color : blue')
-                const p = path_viewer(new_node)
-                console.log(`%cPATH: ${p}`,'color:gray')
-                console.log(parent)
-                // *===
-
+                while(s){
+                    if(s.next == null){
+                        new_node.id = `${parent_id}${child_id+1}` 
+                        new_node.prev = s
+                        console_view({parent:parent,node:new_node,new_child:false}) //* CONSOLE VIEW
+                        return s.next = new_node
+                    }
+                    child_id+=1
+                    s = s.next
+                }
                 return s.next = new_node
             }else{
                 new_node.prev = parent
-
-                // * CONSOLE VIEW
-                console.log(`%c PARENT NODE => ${parent.data}\n\t Got First \n\t\t CHILD NODE => ${data} : SUCCESS ✔`,'color : green')
-                const p = path_viewer(new_node)
-                console.log(`%cPATH: ${p}`,'color:gray')
-                console.log(parent)
-                // *===
-
+                new_node.id = `${parent_id}0`
+                console_view({parent:parent,node:new_node,new_child:true}) //* CONSOLE VIEW
                 return parent.child = new_node
             }
         }
 
-        const loop_child = ({data,node}) =>{
-            let n = node
-            while(n){
-                if(n.data === data){
-                    return n
-                }else if(n.child){
-                    let c = loop_child({data : data,node : n.child})
-                    if(c){
-                        return c
-                    }
-                }
-                n = n.next
-            }
-            return false
-        }
+        // const loop_child = ({data,node}) =>{
+        //     let n = node
+        //     while(n){
+        //         if(n.data === data){
+        //             return n
+        //         }else if(n.child){
+        //             count+=1
+        //             let c = loop_child({data : data,node : n.child})
+        //             if(c){
+        //                 return c
+        //             }
+        //         }
+        //         n = n.next
+        //     }
+        //     return false
+        // }
 
         while(current){
-
             if(parent){
                 if(current.data === parent) return make_node({parent : current,new_node : node})
                 let c_child
                 if(current.child)c_child = current.child
                 
                 if(c_child){
-                    const o = loop_child({data : parent, node : c_child})
+                    // todo: THIS HAS TO BE **ID BASED** SEARCH IN ORDER TO CREATE A CHILD NODE FOR THE TARGETED NODE
+                    const o = this.search({data : parent, node : c_child})
                     if(o){
                         current = o
                         continue
@@ -107,33 +128,35 @@ class Branch{
             }else{
                 if(!current.next){
                     node.prev = current
-
-                    // * CONSOLE VIEW
-                    console.log(`%c Branch ${node.prev.data}\n\t Got Next \n\t\t Branch => ${data} : SUCCESS ✔`,'color : brown')
-                    const p = path_viewer(node)
-                    console.log(`%cPATH: ${p}`,'color:gray')
-                    console.log(current)
-                    // *===
-
+                    console_view({parent:node.prev,node:node,child:false})//* CONSOLE VIEW
+                    node.id = count
                     return current.next = node
                 }
             }
-
+            count+=1
             current = current.next
         }
     }
 
-    search({data, node,next = true}){
-        let current = node
+    search({id,data,node}){
+        let n = this.head
+        const datas = []
+        if(node)n=node
+        if(data && id)return 'Either Data Or ID not both searching available for now'
+        while(n){
+            if(id && n.id === id)return n
+            if(data && n.data === data){
+                datas.push(n)
+                return n
+            }
 
-        while(current){
-            // console.log(current,data)
-            if(data && current.data === data) return current
-            if(!data && current.next === null)return current
-            if(next)current = current.next;else current = current.child
-            // current = current.next
+            if(n.child){
+                let s2 = this.search({data:data,node:n.child})
+                if(id)s2 = this.search({id:id,node:n.child})
+                if(s2)return s2
+            }
+            n = n.next
         }
-
         return false
     }
 }
@@ -159,7 +182,7 @@ b.append({parent:'stud1',data:'paper1'})
 b.append({parent:'paper1',data:'paper11'})
 
 // ? CS -> PHD -> STUD1 -> PAPER1 -> title
-b.append({parent:'paper11',data:'title'})
+// b.append({parent:'paper11',data:'title'})
 
 // ? CS -> PHD -> STUD1 -> PAPER1 -> author
 b.append({parent:'paper11',data:'author'})
@@ -211,4 +234,19 @@ b.append({parent:'pro2',data:'name02'})
 // ? CS -> PRO -> PRO2 -> PAPER02
 b.append({parent:'pro2',data:'paper02'})
 
+
+// !NEXT
+// b.append({data:'c'})
+// b.append({parent:'c',data:'c++'})
+// b.append({parent:'c',data:'c#'})
+// b.append({parent:'c',data:'java'})
+// b.append({parent:'c',data:'python'})
+// b.append({parent:'c',data:'dart'})
+// b.append({parent:'java',data:'javascript'})
+// b.append({parent:'javascript',data:'nodejs'})
+console.log('')
+
 console.log(b)
+const id_search = b.search({id:'102100'})
+console.log(id_search)
+
